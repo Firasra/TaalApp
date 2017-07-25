172fae3e384bcdf29ae415286e722e87b778da3f
Firas Rassas <firas_ra>
2017-07-25 20:08:00 +0300

    seperate controllers and views from each other

---
+++ www/js/Controllers/homeController.js
@@ -0,0 +1,60 @@
+$(document).ready(function(){
+
+  isLoggedin();
+  changeLanguage();
+  startScanning();
+
+});
+
+function loggedin(){
+    
+}
+
+function notLoggedin(){
+    logout();
+}
+
+
+function startScanning() {     
+
+    $('#scan').click( function() {
+          cordova.plugins.barcodeScanner.scan(
+          function (result) {
+             alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+              var token    = typeof localStorage.auth_token !== 'undefined' ? localStorage.auth_token : '';
+              var username = typeof localStorage.username !== 'undefined' ? localStorage.username : '';
+              $.ajax({
+                type: "GET", 
+                crossDomain: true, 
+                url: serverSite+"api/site?site="+parseInt(result.text),
+                dataType: 'json', 
+                cache: false, 
+                headers: {"username":username, "token":token},
+                data: {"site": 1}
+              }).done(function() {
+               //
+              });           
+          }, 
+          function (error) {
+              alert("Scanning failed: " + error);
+          },
+          {
+              preferFrontCamera : false, // iOS and Android
+              showFlipCameraButton : true, // iOS and Android
+              showTorchButton : true, // iOS and Android
+              torchOn: false, // Android, launch with the torch switched on (if available)
+              prompt : translation[choosedLanguage]['PLACE_QRCODE'], // Android
+              resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+              formats : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
+              orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+              disableAnimations : false, // iOS
+              disableSuccessBeep: false // iOS
+          });
+        }
+     );
+}
+
+
--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,3 @@
-$(document).ready(function(){
-
- // startScanning();
-  isLoggedin();
-  changeLanguage();
-
-  // document.addEventListener("deviceready", onDeviceReady, false);
-});
-
 function getFormParams(form){
   var data = form.serializeArray().reduce(function(obj, item) {
                   obj[item.name] = item.value;
@@ -61,26 +52,22 @@ function logoutAction(){
 }
 
 function isLoggedin(){
-  var path = window.location.pathname;
-  var page = path.split("/").pop();
-  if(page !== 'login.html' && page !== 'signup.html'){
-    var token    = typeof localStorage.auth_token !== 'undefined' ? localStorage.auth_token : '';
-    var username = typeof localStorage.username !== 'undefined' ? localStorage.username : '';
-    $.ajax({
-      type: "POST", 
-      crossDomain: true, 
-      url: serverSite+"api/isLoggedin", 
-      dataType: 'json', 
-      cache: false, 
-      headers:{ "username" : username, "token": token } 
-    }).done(function(response) {
-      if (! (typeof response.success !== 'undefined' && response.success) ) {
-        logout();
-      }else{
-        loadPage('home.html');
-      }
-    }); 
-  }
+  var token    = typeof localStorage.auth_token !== 'undefined' ? localStorage.auth_token : '';
+  var username = typeof localStorage.username !== 'undefined' ? localStorage.username : '';
+  $.ajax({
+    type: "POST", 
+    crossDomain: true, 
+    url: serverSite+"api/isLoggedin", 
+    dataType: 'json', 
+    cache: false, 
+    headers:{ "username" : username, "token": token } 
+  }).done(function(response) {
+    if (! (typeof response.success !== 'undefined' && response.success) ) {
+      notLoggedin();
+    }else{
+      loggedin();
+    }
+  }); 
 }
 
 function logout(){
@@ -91,10 +78,7 @@ function logout(){
 
 // load html template content
 function loadPage(wantedPage){
-  $("#page_content").load(wantedPage, function(){
-    changeLanguage();
-    startScanning();
-  });
+  window.location.href = "/" + wantedPage;
 }
 
 // change displayed language
@@ -116,47 +100,3 @@ function changeLanguage(language){
     
   })
 }
-
-function startScanning() {     
-
-    $('#scan').click( function() {
-          cordova.plugins.barcodeScanner.scan(
-          function (result) {
-             alert("We got a barcode\n" +
-                "Result: " + result.text + "\n" +
-                "Format: " + result.format + "\n" +
-                "Cancelled: " + result.cancelled);
-              var token    = typeof localStorage.auth_token !== 'undefined' ? localStorage.auth_token : '';
-              var username = typeof localStorage.username !== 'undefined' ? localStorage.username : '';
-              $.ajax({
-                type: "GET", 
-                crossDomain: true, 
-                url: serverSite+"api/site?site="+parseInt(result.text),
-                dataType: 'json', 
-                cache: false, 
-                headers: {"username":username, "token":token},
-                data: {"site": 1}
-              }).done(function() {
-               //
-              });           
-          }, 
-          function (error) {
-              alert("Scanning failed: " + error);
-          },
-          {
-              preferFrontCamera : false, // iOS and Android
-              showFlipCameraButton : true, // iOS and Android
-              showTorchButton : true, // iOS and Android
-              torchOn: false, // Android, launch with the torch switched on (if available)
-              prompt : translation[choosedLanguage]['PLACE_QRCODE'], // Android
-              resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-              formats : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
-              orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-              disableAnimations : false, // iOS
-              disableSuccessBeep: false // iOS
-          });
-        }
-     );
-}
-
-
